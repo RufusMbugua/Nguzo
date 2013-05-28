18eb7ec2073b0b6242bc77e73ffff512ba242412
Rufus Ngugi Mbugua <mbuguarufus@gmail.com>
2013-05-28 05:34:27 +0300

    Redesign

--- application/views/sections/header.php
+++ application/views/sections/header.php
@@ -1,3 +1,18 @@
-<section class="banner">
-	
-</section>
+<nav class="page-navigation">
+	<section class="logo">
+		nguzo
+	</section>
+
+	<ul class="page-links">
+		<li>
+			<a>Home</a>
+		</li>
+		<li>
+			<a>Class</a>
+		</li>
+		<li>
+			<a>Lesson</a>
+		</li>
+	</ul>
+</nav>
+
